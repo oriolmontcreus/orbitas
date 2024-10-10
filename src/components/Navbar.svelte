bc4e8cb3416b3132992208f4702019bf51e78fc1
oriolmontcreus <licensedgamer641319@gmail.com>
2024-10-10 20:01:53 +0200

    style: minor navbar style changes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
-  import { isUserLoggedIn, getCurrentUser } from '@services/auth';
-  
+  import { isUserLoggedIn, getCurrentUser } from "@services/auth";
+
   let isLoading = true;
   let isLoggedIn = false;
   let user = null;
 
   $: (() => {
-    // Simulate a delay to mimic async auth check
-    setTimeout(() => {
-      isLoggedIn = isUserLoggedIn();
-      if (isLoggedIn) {
-        user = getCurrentUser();
-      }
-      isLoading = false;
-    }, 500);
+    isLoggedIn = isUserLoggedIn();
+    if (isLoggedIn) user = getCurrentUser();
+    isLoading = false;
   })();
 </script>
 
@@ -25,17 +20,21 @@
   <div class="flex items-center">
     {#if isLoading}
       <Button variant="outline" disabled>
-        <div class="w-5 h-5 border-4 border-t-gray-500 border-gray-300 rounded-full animate-spin"></div>
+        <div
+          class="w-5 h-5 border-4 border-t-gray-500 border-gray-300 rounded-full animate-spin"
+        ></div>
       </Button>
     {:else if isLoggedIn && user}
       <Button variant="outline">
         <span>@{user.username}</span>
-        <img src={user.avatar || ''} alt="Avatar" class="w-7 h-7 ml-2 rounded-full border-2 border-gray-700" />
+        <img
+          src={user.avatar || ""}
+          alt="Avatar"
+          class="w-7 h-7 ml-2 rounded-full border-2 border-gray-700"
+        />
       </Button>
     {:else}
-      <Button href="/login" variant="outline">
-        Login
-      </Button>
+      <Button href="/login" variant="outline">Login</Button>
     {/if}
   </div>
 </nav>
